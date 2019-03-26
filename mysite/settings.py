a8de21babb8c1e8a71f38716a28555f5eb3c1f66
josegarban <36708774+josegarban@users.noreply.github.com>
2019-03-26 16:18:18 -0400

    Modified settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,6 +120,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, STATIC_ROOT, "/blog", STATIC_URL),
-#    ]
+STATICFILES_DIRS = (
+    normpath(join(BASE_DIR, 'static')),
+)
