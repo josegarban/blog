7c5c84d4a15889b793ccd3e83644e558c2a3b524
josegarban <36708774+josegarban@users.noreply.github.com>
2019-04-02 21:46:56 -0400

    added app name = blog at the beginning

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
     path('', views.post_list, name='post_list'), #root
     path('about/', views.about, name='about'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          views.post_detail,
          name='post_detail'),
-    path('new/', views.post_new, name='post_new'),
+    path('post/new/', views.post_new, name='post_new'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          views.post_edit,
          name='post_edit'),
